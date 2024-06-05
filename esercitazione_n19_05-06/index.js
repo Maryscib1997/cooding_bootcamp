61b93acaff51cf92f6cb0e13b0fdb24750aea85a
Mary Scibilia <166290846+Maryscib1997@users.noreply.github.com>
2024-06-05 18:34:09 +0200

    Add files via upload

---
+++ esercitazione_n19_05-06/index.js
@@ -0,0 +1,26 @@
+// Funzione globale che logga il contesto (this) e l'elemento target (event.target)
+function logContext(event) {
+    console.log('This:', this);
+    console.log('Event Target:', event.target);
+}
+
+const divParent = document.querySelector(".container");
+const button1El = document.querySelector(".button1");
+const button2El = document.querySelector(".button2");
+const button3El = document.querySelector(".button3");
+
+// EVENT LISTENER in ascolto su ogni singolo button element
+button1El.addEventListener("click", function(event) {
+    console.log("sono button1");
+    logContext.call(this, event);
+});
+
+button2El.addEventListener("click", function(event) {
+    console.log("sono button2");
+    logContext.call(this, event);
+});
+
+button3El.addEventListener("click", function(event) {
+    console.log("sono button3");
+    logContext.call(this, event);
+});
