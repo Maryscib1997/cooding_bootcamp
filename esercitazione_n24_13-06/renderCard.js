0cae47d10084c60e41830f3058ae3c5a2d96868b
Mary Scibilia <166290846+Maryscib1997@users.noreply.github.com>
2024-06-13 18:18:08 +0200

    Add files via upload

---
+++ esercitazione_n24_13-06/renderCard.js
@@ -0,0 +1,24 @@
+
+// Funzione che renderizza una singola card
+export const renderCard = (movie, container) => {
+    const productCard = document.createElement('div');
+        const title = document.createElement('p');
+        const productImage = document.createElement('img');
+        productCard.classList.add('product-card');
+
+        title.textContent = movie.title ?? movie.name;
+        productImage.src = `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`;
+  
+      productCard.append(productImage, title);
+      container.append(productCard);
+  }
+
+  // funzione di renderList, pulisce il container e renderizza un array di cards
+  export const renderList = (dataList, container) => {
+    container.innerHTML = "";
+
+    dataList.forEach((item) => {
+      //passiamo tutto l'oggetto movie a renderCard
+        renderCard(item, container);
+      });
+  }
