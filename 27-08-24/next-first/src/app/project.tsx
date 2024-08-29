d5dba37ff2bdfcd71da7baf69a428737f4faa116
Maryscib1997 <mariadomenicascibilia97@gmail.com>
2024-08-29 19:17:02 +0200

    prova

---
+++ 27-08-24/next-first/src/app/project.tsx
@@ -0,0 +1,12 @@
+// src/app/project.tsx
+import React from 'react';
+
+const Project: React.FC = () => {
+  return (
+    <div>
+      <h1>Il Progetto</h1>
+    </div>
+  );
+};
+
+export default Project;
